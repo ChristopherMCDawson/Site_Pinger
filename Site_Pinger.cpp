bd310fc954b7073606ac9f82d55631464bf027a7
Christopher Decarie-Dawson <Decariechris@gmail.com>
2024-05-12 13:59:17 -0400

    Add project files.

---
+++ Site_Pinger.cpp
@@ -0,0 +1,12 @@
+﻿// Site_Pinger.cpp : Defines the entry point for the application.
+//
+
+#include "Site_Pinger.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
